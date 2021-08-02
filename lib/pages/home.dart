232bd603e2aad7b9eb2f3ce3f14328f31e0c30cb
KarikariAdade <juniorlecrae@gmail.com>
2021-08-02 21:08:33 +0000

    added weather images

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,41 +20,56 @@ class _HomeState extends State<Home> {
 
     data = ModalRoute.of(context).settings.arguments;
 
-    print(data);
+    // Set background
+
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+
+    var textColor = data['isDaytime'] ? Colors.black : Colors.white;
+
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(10.0, 150.0, 10.0, 10.0),
-          child: Column(
-            children: <Widget>[
-              FlatButton.icon(
-                  onPressed: (){
-                    Navigator.pushNamed(context, '/location');
-                  },
-                  icon: Icon(Icons.edit_location),
-                  label: Text('Edit Location')
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    data['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(height: 20.0),
-              Text(
-                data['time'],
-                style: TextStyle(
-                  fontSize: 66.0
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover
+            )
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(10.0, 150.0, 10.0, 10.0),
+            child: Column(
+              children: <Widget>[
+                FlatButton.icon(
+                    onPressed: (){
+                      Navigator.pushNamed(context, '/location');
+                    },
+                    icon: Icon(Icons.edit_location, color: textColor),
+                    label: Text('Edit Location', style: TextStyle(color: textColor))
                 ),
-              )
-            ],
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: textColor
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: textColor,
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ),
